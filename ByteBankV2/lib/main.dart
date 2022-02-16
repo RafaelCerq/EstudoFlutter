484a1f56306aaa2b9d1bae9c9c74d65a5207658b
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-02-16 17:55:37 -0300

    Ajustando visual dashboard

--- ByteBankV2/lib/main.dart
+++ ByteBankV2/lib/main.dart
@@ -1,32 +1,76 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankApp());
 
 class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
+        theme: ThemeData(
+            primaryColor: Colors.green[900],
+            colorScheme: ColorScheme.fromSwatch()
+                .copyWith(secondary: Colors.blueAccent[700]),
+            buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary,
+            )
         ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height: 120,
-              width: 100,
-              color: Colors.green,
+        home: Dashboard()
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Dashboard'),
+          backgroundColor: Colors.green[900]
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme
+                  .of(context)
+                  .primaryColor,
               child: Column(
-                children: <Widget>[
-                  Icon(Icons.people),
-                  Text('Contacts'),
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text('Contacts',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ))
                 ],
               ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
 }
+
