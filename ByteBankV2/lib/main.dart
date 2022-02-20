0ba9940ec3655eca43138a0a925e6c909fd5050d
unknown <rafaelcerq91@gmail.com>
2022-02-19 22:33:58 -0300

    Integrando telas e banco

--- ByteBankV2/lib/main.dart
+++ ByteBankV2/lib/main.dart
@@ -1,76 +1,41 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, use_key_in_widget_constructors
 
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(BytebankApp());
+import 'database/app_database.dart';
+import 'models/contact.dart';
+
+void main() {
+  runApp(BytebankApp());
+
+}
 
 class BytebankApp extends StatelessWidget {
-  const BytebankApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-            primaryColor: Colors.green[900],
-            colorScheme: ColorScheme.fromSwatch()
-                .copyWith(secondary: Colors.blueAccent[700]),
-            buttonTheme: ButtonThemeData(
-              buttonColor: Colors.blueAccent[700],
-              textTheme: ButtonTextTheme.primary,
-            )
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.green.shade900,
+            secondary: Colors.green[700],
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent.shade700),
+              )
+          ),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          )
         ),
         home: Dashboard()
     );
   }
 }
 
-class Dashboard extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('Dashboard'),
-          backgroundColor: Colors.green[900]
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme
-                  .of(context)
-                  .primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text('Contacts',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ))
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
 
